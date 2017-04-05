17f0082812cd59819ec3fe4742e47b789249a66f
Rapier97 <alex.rapier13@gmail.com>
2017-04-05 19:40:23 -0400

    add emoji

---
+++ ContactListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactListTableViewController.h
+//  MidtermProject
+//
+//  Created by Alex Rapier on 05/04/2017.
+//  Copyright © 2017 MicroBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactListTableViewController : UITableViewController
+
+@end
